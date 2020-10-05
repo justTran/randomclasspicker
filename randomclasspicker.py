eee35d0f38d29aae8ce6ce462ba9be3c991685de
Elijah Elrod <30705700+ElijahElrod@users.noreply.github.com>
2020-10-04 20:13:45 -0400

    Update randomclasspicker.py
    Added more fine-grained control on elective choices depending on requirements for graduation.

--- randomclasspicker.py
+++ randomclasspicker.py
@@ -1,10 +1,12 @@
 import random
 
-clist = ["24483	CDA	4321    Cryptographic Hardware	        BSCS	Hardware",
+theory_clist = ["15599	COT	4210	Automata Thry/Formal Languages	BSCS	Theory",
+        "18171	COT	4521	Computational Geometry	        BSCS	Theory"]
+hardware_clist = ["24483	CDA	4321    Cryptographic Hardware	        BSCS	Hardware",
         "18749	CDA	4621    Control of Mobile Robots	    BSCS	Hardware",
         "13595	CDA	4253	Field Prgm Gate Array Design	BSCS	Hardware",
-        "12594	CAP	4401	Image Processing Fundamentals	BSCS	Hardware",
-        "23670	COP	4710	Database Design	                BSCS	Software",
+        "12594	CAP	4401	Image Processing Fundamentals	BSCS	Hardware"]
+software_clist = ["23670	COP	4710	Database Design	                BSCS	Software",
         "16799	COP	4365	Software System Development	    BSCS	Software",
         "17154	COP	4656	Software Devl Mobile Devices	BSCS	Software",
         "11058	CIS	4930	Capture the Flag	            BSCS	Software",
@@ -12,14 +14,20 @@ clist = ["24483	CDA	4321    Cryptographic Hardware	        BSCS	Hardware",
         "18174	CIS	4930	Comp Methods for Imaging	    BSCS	Software",
         "13474	CIS	4930	Quantum Computing	            BSCS	Software",
         "15729	COP	4710	Database Design	                BSCS	Software",
-        "22785	CIS	4930	Privacy-Preserving Infras.	    BSCS	Software",
-        "11270	CIS	4930	Intro to Hadoop and Big Data	BSCS	Software",
-        "15599	COT	4210	Automata Thry/Formal Languages	BSCS	Theory",
-        "18171	COT	4521	Computational Geometry	        BSCS	Theory"]
+        "22785	CIS	4930	Privacy-Preserving Infras.	    BSCS	Software"]
 
-if not clist:
+if not software_clist or not hardware_clist or not theory_clist:
     pass
 
 else:
-    print("\nI have chosen this class:")
-    print(clist[random.randint(0, len(clist) - 1)])
+    elective_choice = input("Pick a elective type to get randomly assigned a class (theory, software, hardware): ")
+    if (elective_choice == 'theory'):
+        print("We selected this just for you")
+        print(theory_clist[random.randint(0, len(theory_clist) - 1)])
+    elif(elective_choice == 'hardware'):
+        print("We selected this just for you")
+        print(hardware_clist[random.randint(0, len(hardware_clist) - 1)])
+    else:
+        print("We selected this just for you")
+        print(software_clist[random.randint(0, len(software_clist) - 1)])
+        
